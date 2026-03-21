0529375d4221c18dd6957c2bb1d890b2f6e9ea14
ArianaAguado <ariana.aguado@educa.madrid.org>
2026-03-21 13:07:14 +0100

    estructura inicial del proyecto

---
+++ src/app/app/environments/environments.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-environments',
+  imports: [],
+  templateUrl: './environments.html',
+  styleUrl: './environments.css',
+})
+export class Environments {}
