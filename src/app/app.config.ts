42ca349ddbc039eb0d98a9e73e197679160365f1
ArianaAguado <ariana.aguado@educa.madrid.org>
2026-03-22 10:53:17 +0100

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+// 1. Importaciones necesarias de Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+// 2. Importa tu configuración de entorno
+import { environment } from '../environments/environments'; 
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(), 
+    provideRouter(routes),
+
+    // 3. Inicialización de Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+
+    // 4. Proveedores de servicios específicos
+    provideAuth(() => getAuth()),       // Para el login de usuariosvale, 
+    provideFirestore(() => getFirestore()) // Para guardar los eventos del calendario
+  ],
 };
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
+/*
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-environments',
   imports: [],
@@ -7,3 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './environments.css',
 })
 export class Environments {}
+*/
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDqmTR9t7ic-BZpWnPbaQQ80U3DafnLPeA",
+    authDomain: "tfg-calendario-b73f0.firebaseapp.com",
+    projectId: "tfg-calendario-b73f0",
+    storageBucket: "tfg-calendario-b73f0.firebasestorage.app",
+    messagingSenderId: "590856471833",
+    appId: "1:590856471833:web:02d87951a9bfb2ff87f188",
+    measurementId: "G-H83TYWMJK4"
+  }
+};
