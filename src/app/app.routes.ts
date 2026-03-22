48d2f5d440bfca7d296eaba6f713db41650a6c7d
JavierHerrera2 <jherrera0044@gmail.com>
2026-03-22 11:08:59 +0100

    Prueba de que firebase esta conectado con el proyecto, ademas de añadir al app routes el registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register'; 
 
 export const routes: Routes = [
-  { path: '', component: Login }
+  { path: '', component: Login },           
+  { path: 'register', component: Register }
 ];
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore'; // Importamos Firebase
 
 @Component({
   selector: 'app-register',
+  standalone: true, // Asegúrate de que esto esté así
   imports: [],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  // Inyectamos el servicio de base de datos
+  private firestore = inject(Firestore);
+
+  // Función para probar si la conexión funciona
+  async probarConexion() {
+    try {
+      const col = collection(this.firestore, 'test_tfg');
+      await addDoc(col, { 
+        mensaje: '¡Conectado desde el Register!',
+        fecha: new Date().toISOString() 
+      });
+      alert('¡CONEXIÓN EXITOSA! Revisa tu consola de Firebase.');
+    } catch (error) {
+      console.error('Error al conectar:', error);
+      alert('Fallo en la conexión. Mira la consola (F12).');
+    }
+  }
+}
