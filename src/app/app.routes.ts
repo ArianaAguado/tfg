f5c04afc1d7faf709dfdfb603f03726e5ce5ddb3
ArianaAguado <ariana.aguado@educa.madrid.org>
2026-03-25 11:01:12 +0100

    página de inicio básica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Inicio } from './inicio/inicio';
 
 export const routes: Routes = [
-  { path: '', component: Login }
+  { path: '', component: Inicio },
+  { path: 'login', component: Login }
 ];
