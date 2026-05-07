6be2f5b6cd4d9f7e748ab9ffb63dcbaaa8f61be2
ArianaAguado <ariana.aguado@educa.madrid.org>
2026-05-07 12:18:24 +0200

    creado el guard para evitar que la gente entre a la app sin pasar por el login

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { FirebaseService } from "../services/firebase.service";
+
+export const authGuard: CanActivateFn= () => {
+    const firebase=inject(FirebaseService);
+    const router=inject(Router);
+
+    if (firebase.usuarioActual){
+        return true;
+    } 
+
+    router.navigate(['/login']);
+    return false;
+};
