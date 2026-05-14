e309384b960b61be8a4e60ce29ca3601526763ad
JavierHerrera2 <jherrera0044@gmail.com>
2026-05-14 10:05:32 +0200

    Correcion con las notificaciones y cerrar sesion

--- src/app/components/cerrar-sesion/cerrar-sesion.ts
+++ src/app/components/cerrar-sesion/cerrar-sesion.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseService } from '../../services/firebase.service';
+import { Notificaciones } from '../notificaciones/notificaciones';
 
 @Component({
   selector: 'app-btn-cerrar-sesion',
   standalone: true,
+  imports: [Notificaciones],
   templateUrl: './cerrar-sesion.html',
   styleUrl: './cerrar-sesion.css',
 })
